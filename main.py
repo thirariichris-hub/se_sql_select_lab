0cf776c6b499e4426eaa7de1f93b6be18c6d4810
christian joseph <thirariichris@gmail.com>
2026-04-22 09:46:45 +0300

    created a simple connectin to a database to access employees

--- main.py
+++ main.py
@@ -1,39 +1,26 @@
-# STEP 1A
-# Import SQL Library and Pandas
-
-# STEP 1B
-# Connect to the database
-conn = None
+import sqlite3
+import pandas as pd
 
 
-# STEP 2
-# Replace None with your code
-df_first_five = None
+conn = sqlite3.connect('data.sqlite')
 
-# STEP 3
-# Replace None with your code
-df_five_reverse = None
 
-# STEP 4
-# Replace None with your code
-df_alias = None
 
-# STEP 5
-# Replace None with your code
-df_executive = None
+df_first_five = pd.read_sql("""SELECT employeeNumber, lastName FROM employees""", conn)
 
-# STEP 6
-# Replace None with your code
-df_name_length = None
+df_five_reverse = pd.read_sql("""SELECT lastName, employeeNumber FROM employees""", conn)
 
-# STEP 7
-# Replace None with your code
-df_short_title = None
 
-# STEP 8
-# Replace None with your code
-sum_total_price = None
+df_alias = pd.read_sql("""SELECT lastName, employeeNumber AS ID FROM employees""", conn)
 
-# STEP 9
-# Replace None with your code
-df_day_month_year = None
+df_executive = pd.read_sql("""SELECT *, CASE WHEN jobTitle IN ('President', 'VP Sales', 'VP Marketing') THEN 'Executive' ELSE 'Not Executive' END AS role FROM employees""", conn)
+
+
+df_name_length = pd.read_sql("""SELECT LENGTH(lastName) AS name_length FROM employees""", conn)
+
+df_short_title = pd.read_sql("""SELECT SUBSTR(jobTitle, 1, 2) AS short_title FROM employees""", conn)
+
+sum_total_price = pd.read_sql("""SELECT SUM(ROUND(priceEach * quantityOrdered)) FROM orderDetails""", conn)
+
+
+df_day_month_year = pd.DataFrame()
